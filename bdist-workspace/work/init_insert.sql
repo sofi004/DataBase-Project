a04355a07e882411cc84e65dd4b26f2fff25368f
joaotomas007 <113645804+joaotomas007@users.noreply.github.com>
2024-05-25 19:26:07 +0100

    Delete bdist-workspace directory

--- bdist-workspace/work/init_insert.sql
+++
@@ -1,9 +0,0 @@
---INSERT INTO clinica (nome, telefone, morada)
---VALUES ('sao_joao', '213313313', 'Coimbra');
-
-
---INSERT INTO trabalha (nif, nome, dia_da_semana)
---VALUES ('123456789', 'sao_joao', '0');
-
-INSERT INTO consulta (ssn, nif, nome, data, hora, codigo_sns)
-VALUES ('12345678910', '123456789', 'sao_joao', '2024-06-02', '08:00:00', '123456789011');
